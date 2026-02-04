373df60f1f6ebb59618f25717358ae5ee604dabe
Viola-Case <ajr.acnt@gmail.com>
2026-02-04 13:23:53 -0500

    :)

---
+++ include/ROSE/ROSE_preamble.h
@@ -0,0 +1,14 @@
+/**
+
+    @file      ROSE_preamble.h
+    @brief     Preamble at the beginning of every public header in the ROSE library
+    @details   ~
+    @author    Viola Case
+    @date      4.02.2026
+    @copyright © Cool Guy, 2026. All right reserved.
+
+**/
+#pragma once
+
+#include <cstdlib>
+#include <type_traits>
---
+++ include/ROSE/rtl/ROSE_list.h
@@ -0,0 +1,15 @@
+/**
+
+    @file      ROSE_list.h
+    @brief     
+    @details   ~
+    @author    Viola Case
+    @date      4.02.2026
+    @copyright © Cool Guy, 2026. All right reserved.
+
+**/
+#pragma once
+
+namespace ROSE {
+
+}
