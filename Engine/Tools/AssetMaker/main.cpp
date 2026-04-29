72a061ba09b0dab1f9f5107c6a2f1fe9d5a74b13
viola-case <ajr.acnt@gmail.com>
2026-04-29 15:21:53 -0400

    Asset maker init

---
+++ Engine/Tools/AssetMaker/main.cpp
@@ -0,0 +1,14 @@
+/**
+
+    @file      main.cpp
+    @brief     
+    @details   ~
+    @author    Viola Case
+    @date      29.04.2026
+    @copyright © Viola Case, 2026. All right reserved.
+
+**/
+
+int main() {
+    return 0;
+}
