f41353076c2d60bffc8f5b8e6fea4ff60e7bc06b
Viola-Case <ajr.acnt@gmail.com>
2026-05-11 03:39:19 -0400

    Update main.cpp

--- Engine/Tools/AssetMaker/main.cpp
+++ Engine/Tools/AssetMaker/main.cpp
@@ -12,6 +12,7 @@
 #include <ROSE/ROSE.h>
 
 #include "assetmaker_version.h"
+#include "ROSE/Editor/ROSE_assetfile.h"
 
 #include <cli/CLI.hpp>
 
@@ -34,6 +35,7 @@ int main(int argc, char **argv) {
 
   std::string filename;
   std::string type;
+  std::string output;
 
   bool i;
 
@@ -42,6 +44,7 @@ int main(int argc, char **argv) {
   app.add_option("-f,--file", filename, "Specify the path of the file to pack");
   app.add_option("filename", filename)->required(false);
   app.add_option("-t,--type", type, "Specify the type");
+  app.add_option("-o,--output", output, "Specify the output file");
   app.add_flag("-z,--archive", z);
 
 
@@ -55,11 +58,32 @@ int main(int argc, char **argv) {
   }
 
   if (filename.empty()) {
-    // set filename to whatever field is unflagged
+    std::cout << app.help() << std::endl;
+    return 0;
   }
 
+  std::ifstream ifs(filename.c_str(), std::ios::binary);
+  if (!ifs.is_open()) {
+    PrintF("Failed to open {}", filename);
+  }
+
+  size_t size = std::filesystem::file_size(filename);
+
+  char *buf = new char[size] {};
+  // copy file contents to buffer
+  ifs.close();
+
+  if (output.empty()) {
+    auto purefilename = filename.substr(0, filename.find_last_of('.'));
+    output = purefilename + ".roseasset";
+  }
+
+  std::ofstream ofs(output.c_str(), std::ios::binary);
 
 
+  ofs.close();
+
+  delete[] buf;
 
   return 0;
 }
