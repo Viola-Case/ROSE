c9aa410f00998fd64cc7aa00c4f176dfbfcc2be6
viola-case <ajr.acnt@gmail.com>
2026-08-07 04:20:51 -0400

    Start designing game2 example VN
    very very very barebones

---
+++ examples/game2/main.cpp
@@ -0,0 +1,17 @@
+/**
+
+    @file      main.cpp
+    @brief     
+    @details   ~
+    @author    Viola Case
+    @date      31.07.2026
+    @copyright © Viola Case, 2026. All right reserved.
+
+**/
+
+#include "vnstuff.h"
+
+using namespace VNExample;
+int main() {
+  return 0;
+}
---
+++ examples/game2/vnstuff.h
@@ -0,0 +1,26 @@
+/**
+
+    @file      dialogue.h
+    @brief     
+    @details   ~
+    @author    Viola Case
+    @date      31.07.2026
+    @copyright © Viola Case, 2026. All right reserved.
+
+**/
+
+#pragma once
+
+#include <ROSE/ROSE.h>
+
+#include <nlohmann/json.hpp>
+
+#include <SDL3_ttf/SDL_ttf.h>
+
+namespace VNExample {
+  using namespace ROSE;
+
+  
+
+}
+
